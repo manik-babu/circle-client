c608fd5ef8c4b2c55d2ea16cc29308591f54bb60
manik-babu <mdmanikbabu48@gmail.com>
2026-01-19 22:49:20 +0600

    Update proxy for role base route access

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,11 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import userService from "./services/user.service";
 
 export async function proxy(request: NextRequest) {
+    const path = request.nextUrl.pathname;
 
     const session = await userService.getSession();
+
+    // Check user has the session or not
     if (session.data) {
         return NextResponse.redirect(new URL('/', request.url));
     }
+
+
     return NextResponse.next();
 }
 
