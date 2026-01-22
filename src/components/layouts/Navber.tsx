e480afc67be7ad531dfc703a33a60043f4b6f9a7
manik-babu <mdmanikbabu48@gmail.com>
2026-01-22 19:02:14 +0600

    Add ui for home page

--- src/components/layouts/Navber.tsx
+++ src/components/layouts/Navber.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function Navber() {
     const { items: routes } = commonRoutes;
     return (
-        <div className="fixed bottom-4 flex justify-center items-center w-screen h-16">
-            <ul className="relative flex w-xl justify-around items-center bg-gray-800 h-full rounded-md">
+        <div className="fixed bottom-1 flex justify-center items-center w-screen h-16">
+            <ul className="relative flex w-xl justify-around items-center dark:bg-card bg-gray-200 h-full rounded-md">
                 {routes.map((route, index) => (
                     <li key={index}><Link href={route.url}>{route.title}</Link></li>
                 ))}
