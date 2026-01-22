f93dab147d63d17507d4660d57bb51a0d6f7fba3
manik-babu <mdmanikbabu48@gmail.com>
2026-01-22 20:47:05 +0600

    Add dark and light mode

--- src/components/layouts/Navber.tsx
+++ src/components/layouts/Navber.tsx
@@ -6,7 +6,7 @@ export default function Navber() {
     const { items: routes } = commonRoutes;
     return (
         <div className="fixed bottom-1 flex justify-center items-center w-screen h-16">
-            <ul className="relative flex w-xl justify-around items-center dark:bg-card bg-gray-200 h-full rounded-md">
+            <ul className="relative flex w-xl justify-around items-center dark:shadow-theme shadow-theme dark:bg-card bg-card h-full rounded-md">
                 {routes.map((route, index) => (
                     <li key={index}><Link href={route.url}>{route.title}</Link></li>
                 ))}
