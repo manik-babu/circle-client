ed8fda1cad423e316f12bf4461120a333937c435
manik-babu <mdmanikbabu48@gmail.com>
2026-01-25 16:34:00 +0600

    Finish lgoin page

--- src/components/modules/auth/login-form.tsx
+++ src/components/modules/auth/login-form.tsx
@@ -12,17 +12,46 @@ import {
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
-import { signInWithGoogle } from "@/lib/auth-client"
+import { authClient, signInWithGoogle } from "@/lib/auth-client"
+import { useForm } from "@tanstack/react-form"
+import z from "zod";
+
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8, "Must be at least 8 characters long.")
+
+});
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+      const { data, error } = await authClient.signIn.email(value);
+      if (!error) {
+        console.log(data)
+      }
+      else {
+        console.log(error)
+      }
+    },
+
+  })
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
@@ -34,29 +63,56 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={e => { e.preventDefault(); form.handleSubmit(); }}>
             <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                      <Input
+                        id={field.name}
+                        type="email"
+                        placeholder="m@example.com"
+                        onChange={e => field.handleChange(e.target.value)}
+                        value={field.state.value}
+                        aria-invalid={isInvalid}
+                      />
+                      {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                    </Field>
+                  )
+                }}
+              />
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
+
+                  return (
+                    <Field data-invalid={isInvalid}>
+                      <div className="flex items-center">
+                        <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                        <a
+                          href="#"
+                          className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <Input
+                        id={field.name}
+                        type="password"
+                        onChange={e => field.handleChange(e.target.value)}
+                        value={field.state.value}
+                        aria-invalid={isInvalid}
+                      />
+                      {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                    </Field>
+                  )
+                }}
+              />
               <Field>
                 <Button type="submit">Login</Button>
                 <Button onClick={signInWithGoogle} variant="outline" type="button">
